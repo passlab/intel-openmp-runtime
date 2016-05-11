6c71993b027556a3b4f5ff2d7570d53c74edd31f
Yonghong Yan <yanyh15@gmail.com>
2016-05-11 16:17:21 -0400

    Update omp_thread_create_1.c

--- interop_test/omp_thread_create_1.c
+++ interop_test/omp_thread_create_1.c
@@ -44,20 +44,20 @@ int main(int argc, char * argv[])
     int ret_value;
 
     omp_thread_t ompthread_0;
-    omp_thread_create(&ompthread, test_fun, (void*)0, NULL);
+    omp_thread_create(&ompthread_0, test_fun, (void*)0, NULL);
 
     omp_thread_join(&ompthread_0, &ret_value);
     printf("omp_thread 0 return: %d\n", ret_value);
 
     omp_thread_t ompthread_1;
     void * stack = malloc(4098);
-    omp_thread_create(&ompthread, test_fun, (void*)1, stack);
+    omp_thread_create(&ompthread_1, test_fun, (void*)1, stack);
     omp_thread_join(&ompthread_1, &ret_value);
     printf("omp_thread 1 return: %d\n", ret_value);
 
     omp_thread_t ompthread_2;
     void * stack = malloc(4098);
-    omp_thread_create(&ompthread, test_fun, (void*)2, stack);
+    omp_thread_create(&ompthread_2, test_fun, (void*)2, stack);
     omp_thread_join(&ompthread_2, &ret_value);
     printf("omp_thread 2 return: %d\n", ret_value);
 
