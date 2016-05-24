fa40d239b6309282899f9c65e6fcdffc71f2f81b
Chunhua (Leo) Liao <liaoch@llnl.gov>
2016-05-24 11:28:38 -0700

    fix bugs in a test

--- interop_test/omp_thread_create_overhead.c
+++ interop_test/omp_thread_create_overhead.c
@@ -2,10 +2,11 @@
 // input the number of num_user_threadsation
 
 #include <stdlib.h>
+#include <stdio.h>
 #include <pthread.h>
 #include <omp.h>
 #include <sys/timeb.h>
-#include <omp_interop.h>
+//#include <omp_interop.h>
 #include <unistd.h>
 
 /* read timer in second */
@@ -21,6 +22,8 @@ void *test_fun(void *arg){
 }
 
 int main(int argc, char * argv[]) {
+   int num_user_threads;
+   int i, tid;
      if (argc >= 2){
         omp_set_num_threads(atoi(argv[1]));
         num_user_threads = atoi(argv[1]);
@@ -28,22 +31,22 @@ int main(int argc, char * argv[]) {
 
     // omp_set_nested();
     // create 50 threads and put them into threadpool
-    #pragma omp parallel shared(user_thread_id, counter) private(tid) num_threads(50)
+    #pragma omp parallel private(tid) num_threads(50)
     {
         tid = omp_get_thread_num(); 
     }
 
-    int num_user_threads = 100;
+    num_user_threads = 100;
     omp_thread_t omp_threads[num_user_threads];
     pthread_t pthreads[num_user_threads];
 
     // omp_thread_create
     double tt = read_timer();
     for(i=0; i<num_user_threads; i++){
-        omp_thread_create(&omp_threads[i], test_fun, i, NULL);
+        omp_thread_create(&omp_threads[i], test_fun, &i, NULL);
     }
     for(i=0; i<num_user_threads; i++){
-        omp_thread_join(NULL);
+        omp_thread_join(&omp_threads[i], NULL);
     }
     tt = read_timer()-tt;
     printf("omp thread execution time:%f\n", tt);
@@ -51,7 +54,7 @@ int main(int argc, char * argv[]) {
     // pthread_create
     tt = read_timer();
     for(i=0; i<num_user_threads; i++){
-        pthread_create(&pthreads[i], NULL, test_fun, i);
+        pthread_create(&pthreads[i], NULL, test_fun, &i);
     }
     for(i=0; i<num_user_threads; i++) {
         pthread_join(pthreads[i], NULL);
